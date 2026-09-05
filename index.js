a95e3668896adb27c37618c5751767890d5f2d2f
Alexander Cancino <ac223up@student.lnu.se>
2026-09-05 10:46:00 +0200

    Fetch advice from open API for greeting

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const namn = process.argv[2];
-const halsning = `Hej ${namn}!`;
-console.log(halsning);
+async function main() {
+  const namn = process.argv[2];
+
+  const response = await fetch("https://api.adviceslip.com/advice");
+  const data = await response.json();
+  const rad = data.slip.advice;
+
+  console.log(`Hi ${namn}, today's advice for you is: ${rad}`);
+}
+
+main();
